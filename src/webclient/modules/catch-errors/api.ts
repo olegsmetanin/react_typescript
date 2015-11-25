2d2467ebe6beb940e84381c3cdbc8c5571181bf6
artem1 <naumov.artem@gmail.com>
2015-11-25 21:11:26 +0300

    Rename copy-pasted api

--- src/webclient/modules/catch-errors/api.ts
+++ src/webclient/modules/catch-errors/api.ts
@@ -2,7 +2,7 @@ import IHTTPClient from '../../../framework/common/http/IHTTPClient';
 import deduplicate from '../../../framework/client/invoke/deduplicate';
 import ApiCaller from '../../../framework/client/invoke/api';
 
-export default class AuthApi extends ApiCaller {
+export default class CatchErrorApi extends ApiCaller {
 
   throwed(options: {counter: number}) {
     return this._post('/api/throw', options);
